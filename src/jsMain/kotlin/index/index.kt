005aa7d021b5e8cd9813f18552de23950e8fbb48
Jonáš Havelka <jonas.havelka@volny.cz>
2024-01-26 20:30:11 +0100

    Ask before leaving

--- src/jsMain/kotlin/index/index.kt
+++ src/jsMain/kotlin/index/index.kt
@@ -1,12 +1,16 @@
 package index
 
 import app.*
+import kotlinx.browser.window
 import react.create
 import react.dom.client.createRoot
 import web.dom.document
 
 fun main() {
 //    requireAll(require.context("src", true, js("/\\.css$/") as RegExp))
-
+    window.onbeforeunload = {
+        it.preventDefault()
+        "Saved?"
+    }
     document.getElementById("root")?.let { createRoot(it).render(App.create()) }
 }
